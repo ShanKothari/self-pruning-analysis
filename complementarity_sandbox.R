29e6bc5afa965624b8ac5ea82a7be875bb5e30cb
Shan Kothari <quercusacerifolia@gmail.com>
2023-07-28 17:50:47 -0400

    use numbers rather than proportions planted
    +return df rather than list

--- complementarity_sandbox.R
+++ complementarity_sandbox.R
@@ -1,5 +1,8 @@
 ## species is a vector of species in the plot
-## props_planted is a vector of proportions of those species planted within the plot
+## nums_planted is a vector of numbers of those species planted within the plot
+## (we use raw numbers planted rather than proportions because we may
+## want to create simulated plots by combining monocultures
+## so if you just combine those plots via rbind, numbers may exceed 1)
 ## props_alive is a vector of proportions of planted trees of the species that survive
 ## species, props_planted, and props_alive MUST be in the same order
 
@@ -7,11 +10,9 @@
 ## rather than pairing them up later, but I find that it makes things
 ## more intuitive, even if a bit more complicated later on
 
-plot_combos<-function(species,props_planted,props_alive,n_pairs=100){
+plot_combos<-function(species,nums_planted,props_alive,n_pairs=100){
   
-  if(sum(props_planted)!=1){
-    stop("proportions of planted species do not sum to 1")
-  }
+  props_planted<-nums_planted/sum(nums_planted)
   
   ## vector of probabilities for outcomes
   ## each species planted * prob of being still alive
@@ -50,11 +51,11 @@ plot_CCI<-function(sp_plot,mortality_plot){
   ## generate 100 pairs of two species sampled
   ## based on real planting numbers and mortality rates
   sp_pairs<-plot_combos(species=mortality_plot$CodeSp,
-                        props_planted=mortality_plot$prop_planted,
+                        nums_planted=mortality_plot$num_planted,
                         props_alive=mortality_plot$prop_alive)
   
   ## output object with outcomes
-  outcome_list<-list()
+  tree_pair_list<-list()
   
   for(i in 1:nrow(sp_pairs)){
     
@@ -69,7 +70,7 @@ plot_CCI<-function(sp_plot,mortality_plot){
 
       tree_a<-list(CRmax=0,CB=0,CD=0,Bj=0)
       tree_b<-list(CRmax=0,CB=0,CD=0,Bj=0)
-      outcome_list[[i]]<-calculate_CCI(tree_a,tree_b)
+      tree_pair_list[[i]]<-calculate_CCI(tree_a,tree_b)
       
     }
     
@@ -88,7 +89,7 @@ plot_CCI<-function(sp_plot,mortality_plot){
                    CD=sp_plot_sub2$CrownDepth[sample2],
                    Bj=sp_plot_sub2$Bj[sample2])
       
-      outcome_list[[i]]<-calculate_CCI(tree_a,tree_b)
+      tree_pair_list[[i]]<-calculate_CCI(tree_a,tree_b)
       
     }
     
@@ -107,7 +108,7 @@ plot_CCI<-function(sp_plot,mortality_plot){
       
       tree_b<-list(CRmax=0,CB=0,CD=0,Bj=0)
       
-      outcome_list[[i]]<-NA
+      tree_pair_list[[i]]<-NA
 
     }
     
@@ -136,10 +137,12 @@ plot_CCI<-function(sp_plot,mortality_plot){
                    CD=sp_plot_sub2$CrownDepth[sample2],
                    Bj=sp_plot_sub2$Bj[sample2])
       
-      outcome_list[[i]]<-calculate_CCI(tree_a,tree_b)
+      tree_pair_list[[i]]<-calculate_CCI(tree_a,tree_b)
       
     }
   }
   
-  return(outcome_list)
+  tree_pair_df<-do.call(rbind.data.frame, tree_pair_list)
+  colnames(tree_pair_df)<-names(tree_pair_list[[1]])
+  return(tree_pair_df)
 }
