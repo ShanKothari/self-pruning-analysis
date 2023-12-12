ef350b80449447f7aa1b5222d8b15913e3608e6f
Shan Kothari <quercusacerifolia@gmail.com>
2023-12-11 22:01:51 -0500

    split NCI and FD calculations

--- 01 calc_neighbor_vars.R
+++ 01 calc_neighbor_vars.R
@@ -22,7 +22,7 @@ neighbor.finder<-function(dat,outcome.var,sp.var,radius){
     xpos<-dat$X_Pos[i]
     ypos<-dat$Y_Pos[i]
 
-    ## get neighbor IDs
+    ## get IDs of neighbors (within radius)
     distances<-sqrt((dat$X_Pos-xpos)^2+(dat$Y_Pos-ypos[1])^2)
     neighbors<-dat[which(distances <= radius),
                    c(outcome.var,sp.var,"UniqueTreeID")]
@@ -42,89 +42,101 @@ neighbor.finder<-function(dat,outcome.var,sp.var,radius){
   return(outcome.list)
 }
 
+#####################################
+## calculate NCI
+
 ## grab basal areas of neighbors
 ## within radius of 1.1 m
 ## this is 90th percentile of measured crown radii
 ## (concatenating CR1, CR2, CR3, CR4 from self pruning data)
-neighbor.area<-neighbor.finder(Inventory2018,"PV","CodeSp",radius=1.1)
+neighbor.area.NCI<-neighbor.finder(Inventory2018,"PV","CodeSp",radius=1.1)
 
 ## delete focal trees in plots we don't care about
 ## based on plot composition identifiers
 del_species<-c("ACPL","LADE","TICO","PIAB","PISY","QURO","PIOM","BELE","BEPE","PIMA")
 del_plots<-unique(Inventory2018$Plot[Inventory2018$CodeSp %in% del_species])
 del_plot_pattern<-paste(paste("_",del_plots,"_",sep=""),collapse="|")
-neighbor.area<-neighbor.area[-which(grepl(del_plot_pattern,names(neighbor.area)))]
+neighbor.area.NCI<-neighbor.area.NCI[-which(grepl(del_plot_pattern,names(neighbor.area.NCI)))]
 
-## delete neighborhoods with no living trees
-neighbor.total<-unlist(lapply(neighbor.area,
-                              function(neighborhood) sum(neighborhood$outcome,na.rm=T)))
-if(sum(neighbor.total==0)>0){
-  neighbor.area<-neighbor.area[-which(neighbor.total==0)]
+## a function to calculate a simplified NCI
+## (without species-specific parameters)
+simp.NCI<-function(neighborhood,num_exp=1,den_exp=1){
+  
+  ## ignore the focal tree itself
+  cl<-neighborhood[-which(neighborhood$distance==0),]
+  comp.index<-sum(cl$outcome^num_exp/cl$distance^den_exp,na.rm=T)
+  return(comp.index)
 }
 
-########################################
-## calculate various competition indices
-## including a distance-weighted sum
-## and Hegyi (also incorporates focal tree size)
-
-neighbor.comp<-unlist(lapply(neighbor.area,
-                             function(neighborhood){
-                               cl<-neighborhood[-which(neighborhood$distance==0),]
-                               comp.index<-sum(cl$outcome/cl$distance,na.rm=T)
-                               return(comp.index)
-                             }))
+## calculate the simplified NCI for each neighborhood
+neighbor.comp<-unlist(lapply(neighbor.area.NCI,simp.NCI,num_exp=2/3))
 
 ###########################################
-## get community data into the right format(s)
-## for calculating functional diversity
+## calculate functional diversity and CWMs
+
+## we divide this into a separate section and
+## in case we want to use different radii
+## to calculate NCI and neighborhood function
+
+## a radius of 0.8 is enough to include all
+## eight immediate neighbors, but no more
+neighbor.area.FD<-neighbor.finder(Inventory2018,"PV","CodeSp",radius=0.8)
+
+## get rid of the same plots we dropped for NCI calculations
+neighbor.area.FD<-neighbor.area.FD[-which(grepl(del_plot_pattern,names(neighbor.area.FD)))]
 
 ## row bind all the neighborhoods, with a new column
 ## that designates the ID of the focal tree
 ## 'cl' objects omit the focal tree itself (centerless)
-neighbor.join<-bind_rows(neighbor.area, .id = "UniqueTreeID")
+neighbor.join<-bind_rows(neighbor.area.FD, .id = "UniqueTreeID")
 neighbor.join.cl<-neighbor.join[-which(neighbor.join$distance==0),]
-
-neighbor.join$distance<-NULL
-neighbor.join.cl$distance<-NULL
 
 ## for analyses based on numbers of planted trees
 ## rather than biomass
+neighbor.join$planted<-1
 neighbor.join.cl$planted<-1
 
 ## summing trees of the same species in the same neighborhood
 ## because otherwise matrify won't work properly
-neighbor.agg<-aggregate(neighbor.join$outcome,
+neighbor.agg.num<-aggregate(neighbor.join$planted,
+                            by=list(neighbor.join$UniqueTreeID,
+                                    neighbor.join$species),
+                            FUN=sum)
+
+# neighbor.agg.cl<-aggregate(neighbor.join.cl$outcome,
+#                            by=list(neighbor.join.cl$UniqueTreeID,
+#                                    neighbor.join.cl$species),
+#                            FUN=sum)
+
+neighbor.agg.clnum<-aggregate(neighbor.join$planted,
                         by=list(neighbor.join$UniqueTreeID,
                                 neighbor.join$species),
                         FUN=sum)
 
-neighbor.agg.cl<-aggregate(neighbor.join.cl$outcome,
-                           by=list(neighbor.join.cl$UniqueTreeID,
-                                   neighbor.join.cl$species),
-                           FUN=sum)
-
 ## note that this community matrix includes the focal
 ## tree of each neighborhood!
-neighbor.df<-matrify(neighbor.agg)
 neighbor.df.num<-matrify(neighbor.agg.num)
-neighbor.df.cl<-matrify(neighbor.agg.cl)
+# neighbor.df.cl<-matrify(neighbor.agg.cl)
+neighbor.df.clnum<-matrify(neighbor.agg.clnum)
+
 ## make sure columns are in the same order
-neighbor.df.cl<-neighbor.df.cl[,colnames(neighbor.df)]
-## the number of species in the immediate neighborhood
-neighbor.richness<-rowSums(neighbor.df>0)
+neighbor.df.clnum<-neighbor.df.clnum[,colnames(neighbor.df.num)]
+
+## the number of species planted in the immediate neighborhood
+neighbor.richness<-rowSums(neighbor.df.num>0)
 
 #########################################
 ## calculate functional diversity
 
 trait_summary<-read.csv("TraitData/trait_summary.csv")
-trait_summary<-trait_summary[match(colnames(neighbor.df),trait_summary$SpeciesCode),]
+trait_summary<-trait_summary[match(colnames(neighbor.df.num),trait_summary$SpeciesCode),]
 
 core_traits<-trait_summary[,c("LDMC","N","SLA","SRL","WD")]
 rownames(core_traits)<-trait_summary$SpeciesCode
 
 ## calculate CWM of first trait PCA component, omitting the central tree
 ## this gives as the functional identity of the neighborhood
-neighbor.prop<-neighbor.df.cl/rowSums(neighbor.df.cl)
+neighbor.prop<-neighbor.df.clnum/rowSums(neighbor.df.clnum)
 neighbor.CWM1<-as.matrix(neighbor.prop) %*% trait_summary$PC1
 
 ## for trees with no neighbors, assign a neighborhood functional ID of 0
@@ -134,22 +146,19 @@ neighbor.CWM1<-as.matrix(neighbor.prop) %*% trait_summary$PC1
 ## calculate FDis using FD package
 ## including the central tree
 neighbor.FDis<-fdisp(dist(core_traits),
-                     a = as.matrix(neighbor.df))
+                     a = as.matrix(neighbor.df.num))
 
 ## calculate Scheiner's metrics
 neighbor.FTD<-FTD.comm(tdmat=dist(core_traits),
-                       spmat = as.matrix(neighbor.df),
+                       spmat = as.matrix(neighbor.df.num),
                        abund=T)
 
-neighbor.data<-data.frame(UniqueTreeID=names(neighbor.area),
+neighbor.data<-data.frame(UniqueTreeID=names(neighbor.area.NCI),
                           neighbor.richness=neighbor.richness,
                           NCI=neighbor.comp,
                           neighbor.FI1=neighbor.CWM1,
                           FDis=neighbor.FDis$FDis,
                           qDTM=neighbor.FTD$com.FTD$qDTM)
-
-## to do:
-## calculate CWMs of major traits
 
 ## write data
 write.csv(neighbor.data,"IDENTMontrealData/neighborhood_vars.csv")
