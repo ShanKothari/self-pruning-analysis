db86de3bab48a5148d200ffe4efa5a12e8ad7f32
Shan Kothari <quercusacerifolia@gmail.com>
2023-12-18 15:15:19 -0500

    rearrange code for plot-level calculations
    mostly for clarity, and also to 'fix' the A_2N3 BEPA vs BEAL issue

--- 01 calc_neighbor_vars.R
+++ 01 calc_neighbor_vars.R
@@ -163,39 +163,66 @@ neighbor.data<-data.frame(UniqueTreeID=names(neighbor.area.NCI),
 # write.csv(neighbor.data,"IDENTMontrealData/neighborhood_vars.csv")
 
 #################################
-## output mortalities
+## calculate plot-level overyielding
 
+## to correspond with the sampling in the self-pruning project:
 ## remove edge trees
-## to correspond with the sampling in the self-pruning project
+## only blocks A and D
+## only plots with just native species
+## no 12-species plots
 edge.trees<-c(paste(LETTERS[1:8],"1",sep=""),
               paste(LETTERS[1:8],"8",sep=""),
               paste("A",1:8,sep=""),
               paste("H",1:8,sep=""))
 edge_pattern<-paste(edge.trees,collapse="|")
-Inventory2018_center<-Inventory2018[-which(grepl(edge_pattern,Inventory2018$Pos)),]
+Inventory2018_sub<-Inventory2018[-which(grepl(edge_pattern,Inventory2018$Pos)),]
 
-## count a tree as alive if its status is not "dead"
-## include a dummy column to tally up all planted trees within the inner 36
-Inventory2018_center$num_alive<-ifelse(Inventory2018_center$StateDesc!="Dead",yes=1,no=0)
-Inventory2018_center$num_planted<-1
-
-## output mortality summary statistics
-mortality_2018<-aggregate(cbind(num_alive,num_planted)~Block+Plot+CodeSp,
-                          data=Inventory2018_center,
-                          FUN=sum)
-
-# write.csv(mortality_2018,"IDENTMontrealData/mortality_2018.csv",row.names=F)
-
-#########################################
-## output mean basal area in monoculture in 2018
-
-## only blocks A and D
-## only plots with just native species
-## no 12-species plots
-Inventory2018_sub<-subset(Inventory2018_center,Block %in% c("A","D"))
+Inventory2018_sub<-subset(Inventory2018_sub,Block %in% c("A","D"))
 Inventory2018_sub<-subset(Inventory2018_sub,
                           !grepl(del_plot_pattern,UniqueTreeID))
 Inventory2018_sub<-subset(Inventory2018_sub,PlotRichness!=12)
+
+## create a dummy variable with a value of 1 for all planted trees
+Inventory2018_sub$num_planted<-1
+
+## because we start overyielding calculations at the
+## individual level, we want to consider trees to have
+## 'underperformed' if they died. to this end, we create
+## a new basal area column which replaces NAs (for dead
+## trees) with 0s, to use in overyielding calculations
+Inventory2018_sub$BasalArea_0<-Inventory2018_sub$BasalArea
+Inventory2018_sub$BasalArea_0[which(is.na(Inventory2018_sub$BasalArea_0))]<-0
+
+plot_sp_ba<-aggregate(cbind(BasalArea_0,num_planted)~Block+Plot+CodeSp+PlotRichness,
+                      data=Inventory2018_sub,
+                      FUN=sum,na.rm=T)
+
+plot_sp_ba$planted_freq<-plot_sp_ba$num_planted/36
+
+plot_sp_ba_mono<-subset(plot_sp_ba,PlotRichness==1)
+plot_sp_ba$mono<-apply(plot_sp_ba,1,
+                       function(x) {
+                         plot_sp_ba_mono$BasalArea_0[plot_sp_ba_mono$CodeSp==x["CodeSp"] & 
+                                                     plot_sp_ba_mono$Block==x["Block"]]
+                       })
+plot_sp_ba$mono_exp<-plot_sp_ba$mono*plot_sp_ba$planted_freq
+plot_sp_ba$spOY<-(plot_sp_ba$BasalArea_0-plot_sp_ba$mono_exp)*9*10000/1000000
+
+plot_ba<-aggregate(spOY~Block+Plot+PlotRichness,
+                   data=plot_sp_ba,
+                   FUN=sum,na.rm=T)
+
+plot_ba$UniquePlotID<-paste(plot_ba$Block,
+                            plot_ba$Plot,
+                            sep="_")
+
+#########################################
+## output mean basal area in monoculture in 2018
+## as well as mortalities at the species level
+## (all plots and only monocultures)
+
+## count a tree as alive if its status is not "dead"
+Inventory2018_sub$num_alive<-ifelse(Inventory2018_sub$StateDesc!="Dead",yes=1,no=0)
 
 mortality_agg<-aggregate(cbind(num_alive,num_planted)~CodeSp,
                          data=Inventory2018_sub,
@@ -213,36 +240,22 @@ BA_mono_agg<-aggregate(BasalArea~CodeSp,
                        data=Inventory2018_sub_mono,
                        FUN=mean,na.rm=T)
 
-#########################################
-## calculate plot-level overyielding
+###########################################
+## output mortalities (per species per unique plot)
 
-Inventory2018_sub$dummy<-1
-Inventory2018_sub<-Inventory2018_sub[,c("Block","Plot","CodeSp","PlotRichness",
-                                        "BasalArea","dummy")]
-Inventory2018_sub$BasalArea[which(is.na(Inventory2018_sub$BasalArea))]<-0
+## 'fix' a single BEPA misplanted in a BEAL/QURU biculture
+## it is an actual BEPA, but because this BEPA was not sampled
+## in self-pruning analyses from this plot, we can't include
+## it in calculations of complementarity, etc.
+## this also affects functional diversity at the plot scale
+Inventory2018_sub$CodeSp[Inventory2018_sub$Plot=="2N3" & Inventory2018_sub$CodeSp=="BEPA"]<-"BEAL"
 
-plot_sp_ba<-aggregate(.~Block+Plot+CodeSp+PlotRichness,
-                      data=Inventory2018_sub,
-                      FUN=sum,na.rm=T)
+## output mortality summary statistics
+mortality_2018<-aggregate(cbind(num_alive,num_planted)~Block+Plot+CodeSp,
+                          data=Inventory2018_sub,
+                          FUN=sum)
 
-plot_sp_ba$planted_freq<-plot_sp_ba$dummy/36
-
-plot_sp_ba_mono<-subset(plot_sp_ba,PlotRichness==1)
-plot_sp_ba$mono<-apply(plot_sp_ba,1,
-                       function(x) {
-                         plot_sp_ba_mono$BasalArea[plot_sp_ba_mono$CodeSp==x["CodeSp"] & 
-                                                     plot_sp_ba_mono$Block==x["Block"]]
-                       })
-plot_sp_ba$mono_exp<-plot_sp_ba$mono*plot_sp_ba$planted_freq
-plot_sp_ba$spOY<-(plot_sp_ba$BasalArea-plot_sp_ba$mono_exp)*9*10000/1000000
-
-plot_ba<-aggregate(spOY~Block+Plot+PlotRichness,
-                   data=plot_sp_ba,
-                   FUN=sum,na.rm=T)
-
-plot_ba$UniquePlotID<-paste(plot_ba$Block,
-                            plot_ba$Plot,
-                            sep="_")
+# write.csv(mortality_2018,"IDENTMontrealData/mortality_2018.csv",row.names=F)
 
 #########################################
 ## calculate plot-level functional diversity
@@ -252,22 +265,25 @@ plot_ba$UniquePlotID<-paste(plot_ba$Block,
 mortality_2018$UniquePlotID<-paste(mortality_2018$Block,
                                    mortality_2018$Plot,
                                    sep="_")
+## weights are by numbers of planted individuals
 planted_comm<-matrify(mortality_2018[,c("UniquePlotID","CodeSp","num_planted")])
 
 ## functional dispersion in traits
 ## using Laliberte's FDis and Scheiner's qDTM
 ## the latter is not richness-independent
-plot_fdis<-fdisp(dist(core_traits),a = as.matrix(planted_comm))$FDis
-plot.FTD<-FTD.comm(tdmat=dist(core_traits),
+core_traits_sub<-core_traits[colnames(planted_comm),]
+plot_fdis<-fdisp(dist(core_traits_sub),a = as.matrix(planted_comm))$FDis
+plot.FTD<-FTD.comm(tdmat=dist(core_traits_sub),
                    spmat = as.matrix(planted_comm),
                    abund=T)
 
 ## and heterogeneity in shade tolerance
-plot_sth<-fdisp(dist(setNames(trait_summary$shade_tol,trait_summary$SpeciesCode)),
-                a = as.matrix(planted_comm))$FDis
-plot.FTD_STH<-FTD.comm(tdmat=dist(setNames(trait_summary$shade_tol,trait_summary$SpeciesCode)),
-                   spmat = as.matrix(planted_comm),
-                   abund=T)
+shade_tol<-setNames(trait_summary$shade_tol,
+                    trait_summary$SpeciesCode)[colnames(planted_comm)]
+plot_sth<-fdisp(dist(shade_tol),a = as.matrix(planted_comm))$FDis
+plot.FTD_STH<-FTD.comm(tdmat=dist(shade_tol),
+                       spmat = as.matrix(planted_comm),
+                       abund=T)
 
 plot_vars<-data.frame(UniquePlotID=rownames(planted_comm),
                       FDis=plot_fdis,
@@ -277,4 +293,4 @@ plot_vars<-data.frame(UniquePlotID=rownames(planted_comm),
 
 plot_vars$OY<-plot_ba$spOY[match(plot_vars$UniquePlotID,plot_ba$UniquePlotID)]
 
-write.csv(plot_vars,"IDENTMontrealData/plot_vars.csv",row.names=F)
+# write.csv(plot_vars,"IDENTMontrealData/plot_vars.csv",row.names=F)
